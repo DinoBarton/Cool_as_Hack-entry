7338f04416812cadb81dbafdc4c922d8538446e5
bev29rr <ben.varea@gmail.com>
2025-05-24 12:43:32 +0100

    feat: drawMap borders

--- modules/draw.js
+++ modules/draw.js
@@ -1,7 +1,22 @@
+const BORDER_PERCENT = 0.1;
+
 export function drawMap(ctx) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#202124";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = "white";
+
+    ctx.beginPath();
+
+    ctx.moveTo(0, canvas.height * BORDER_PERCENT); 
+    ctx.lineTo(canvas.width, canvas.height * BORDER_PERCENT); 
+    ctx.stroke();
+
+    ctx.moveTo(0, canvas.height * (1 - BORDER_PERCENT)); 
+    ctx.lineTo(canvas.width, canvas.height * (1 - BORDER_PERCENT)); 
+    ctx.stroke();
 }
 
 export function drawObstacles() {
